0de7642653acf2cd199c19cbf7cd0e35874c88fe
mihavilc17-ctrl <miha.vilc17@gmail.com>
2026-06-11 09:08:52 +0200

    Update rpi-camera.py

--- rpi-camera.py
+++ rpi-camera.py
@@ -33,6 +33,8 @@ for _ in range(10):
     request = picam2.capture_request()
     request.release()
 
+import time
+
 picam2.start()
 
 # Stabilizacija
@@ -43,12 +45,21 @@ for _ in range(10):
 frames = np.empty((N_FRAMES, ROI_H, ROI_W, 3), dtype=np.uint8) #? 8bit
 timestamps = np.empty(N_FRAMES, dtype=np.int64)
 
+t_start = time.time()
+
 for i in range(N_FRAMES):
     request = picam2.capture_request()
+    if request is None:
+        print(f"Timeout pri frame {i}!")
+        break
     np.copyto(frames[i], request.make_array("main"))  # direktna kopija brez alokacije
     timestamps[i] = request.get_metadata()["SensorTimestamp"]
     request.release()
 
+    if i % 10 == 0:
+        elapsed = time.time() - t_start
+        print(f"Frame {i}/{N_FRAMES} — {elapsed:.2f}s — trenutni FPS: {i/elapsed:.1f}")
+        
 picam2.stop()
 
 # === ANALIZA NATANČNOSTI ===
